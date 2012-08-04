b113d7c798468da0be0bc7b204b5af52d931882e
Raynos <raynos2@gmail.com>
2012-08-03 22:18:52 -0700

    updated text

--- client/index.js
+++ client/index.js
@@ -35,7 +35,7 @@ function renderTitleAndLink(root) {
 
     githubLink.href = "https://github.com/Raynos/stream-chat"
     title.textContent = "welcome to stream chat!"
-    description.textContent = "Experimental chat room build on streams"
+    description.textContent = "Experimental chat room build on streams. Try joining the test room!"
 
     root.appendChild(title)
     root.appendChild(description)
